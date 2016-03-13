da3b0cb3d54e7fd214bc717d61a43b112b1a75e9
Edgar To <edjopato@gmail.com>
2016-03-13 16:57:12 +0100

    beautify

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-var concat = require('gulp-concat');
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var uglify = require('gulp-uglify');
-var plumber = require('gulp-plumber');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
+var concat = require( 'gulp-concat' );
+var gulp = require( 'gulp' );
+var gutil = require( 'gulp-util' );
+var uglify = require( 'gulp-uglify' );
+var plumber = require( 'gulp-plumber' );
+var sass = require( 'gulp-sass' );
+var sourcemaps = require( 'gulp-sourcemaps' );
 
 var paths = { in : {
     angular: [
@@ -28,77 +28,76 @@ var paths = { in : {
 // https://www.timroes.de/2015/01/06/proper-error-handling-in-gulp-js/
 var gulp_src = gulp.src;
 gulp.src = function() {
-  return gulp_src.apply(gulp, arguments)
-    .pipe(plumber(function(error) {
+  return gulp_src.apply( gulp, arguments )
+    .pipe( plumber( function( error ) {
       // Output an error message
-      gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
+      gutil.log( gutil.colors.red( 'Error (' + error.plugin + '): ' + error.message ) );
       // emit the end event, to properly end the task
-      this.emit('end');
-    })
-  );
+      this.emit( 'end' );
+    } ) );
 };
 
-gulp.task('connect', function () {
-  var connect = require('gulp-connect');
-  connect.server({
+gulp.task( 'connect', function() {
+  var connect = require( 'gulp-connect' );
+  connect.server( {
     root: 'public',
     livereload: true
-  });
-});
+  } );
+} );
 
-gulp.task('angular-init', function () {
-  gulp.src(paths.in.angular)
-    .pipe(concat('angular.min.js'))
-    .pipe(gulp.dest(paths.out.scripts));
-});
+gulp.task( 'angular-init', function() {
+  gulp.src( paths.in.angular )
+    .pipe( concat( 'angular.min.js' ) )
+    .pipe( gulp.dest( paths.out.scripts ) );
+} );
 
-gulp.task('scripts-init', function () {
-  gulp.src(paths.in.scripts)
-    .pipe(sourcemaps.init())
-    .pipe(uglify({
+gulp.task( 'scripts-init', function() {
+  gulp.src( paths.in.scripts )
+    .pipe( sourcemaps.init() )
+    .pipe( uglify( {
       mangle: false
-    }))
-    .pipe(concat('scripts.min.js'))
-    .pipe(uglify({
+    } ) )
+    .pipe( concat( 'scripts.min.js' ) )
+    .pipe( uglify( {
       mangle: false
-    }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.out.scripts));
-});
+    } ) )
+    .pipe( sourcemaps.write( '.' ) )
+    .pipe( gulp.dest( paths.out.scripts ) );
+} );
 
-gulp.task('styles-init', function () {
-  gulp.src(paths.in.styles)
-    .pipe(sourcemaps.init())
-    .pipe(concat('styles.min.css'))
-    .pipe(sass({
+gulp.task( 'styles-init', function() {
+  gulp.src( paths.in.styles )
+    .pipe( sourcemaps.init() )
+    .pipe( concat( 'styles.min.css' ) )
+    .pipe( sass( {
       outputStyle: 'compressed'
-    }).on('error', sass.logError))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.out.styles));
-  gulp.src(paths.in.stylethemes)
-    .pipe(sourcemaps.init())
-    .pipe(sass({
+    } ).on( 'error', sass.logError ) )
+    .pipe( sourcemaps.write( '.' ) )
+    .pipe( gulp.dest( paths.out.styles ) );
+  gulp.src( paths.in.stylethemes )
+    .pipe( sourcemaps.init() )
+    .pipe( sass( {
       outputStyle: 'compressed'
-    }).on('error', sass.logError))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.out.stylethemes));
-});
+    } ).on( 'error', sass.logError ) )
+    .pipe( sourcemaps.write( '.' ) )
+    .pipe( gulp.dest( paths.out.stylethemes ) );
+} );
 
-gulp.task('watch', function () {
-  var watch = require('gulp-watch');
-  watch('js/**/*.*', function () {
-    gulp.start('scripts-init');
-  });
-  watch('style/**/*.*', function () {
-    gulp.start('styles-init');
-  });
-  watch('public/**/*.*', function () {
-    var connect = require('gulp-connect');
-    gulp.src('public/**/*.*')
-      .pipe(connect.reload());
-  });
-});
+gulp.task( 'watch', function() {
+  var watch = require( 'gulp-watch' );
+  watch( 'js/**/*.*', function() {
+    gulp.start( 'scripts-init' );
+  } );
+  watch( 'style/**/*.*', function() {
+    gulp.start( 'styles-init' );
+  } );
+  watch( 'public/**/*.*', function() {
+    var connect = require( 'gulp-connect' );
+    gulp.src( 'public/**/*.*' )
+      .pipe( connect.reload() );
+  } );
+} );
 
-gulp.task('default', ['angular-init', 'scripts-init', 'styles-init']);
+gulp.task( 'default', [ 'angular-init', 'scripts-init', 'styles-init' ] );
 
-gulp.task('dev', ['default', 'connect', 'watch']);
+gulp.task( 'dev', [ 'default', 'connect', 'watch' ] );
