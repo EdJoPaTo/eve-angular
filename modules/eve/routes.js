0cec100162ae89e31a6c4c1b625713ea7c8d6f47
Edgar To <edjopato@gmail.com>
2016-03-14 16:29:03 +0100

    beautify

--- modules/eve/routes.js
+++ modules/eve/routes.js
@@ -1,15 +1,30 @@
-angular.module("eve")
-.config(function($routeProvider) {
-  $routeProvider
-  .when('/', {
-    templateUrl: 'general/page/main.html'
-  })
-  .when('/mining', { redirectTo: '/mining/ore/sell' })
-  .when('/mining/:kind/:pricetype', { templateUrl: 'eve/page/mining.html', controller: 'miningCtrl' })
-  .when('/iteminfo', { templateUrl: 'eve/page/itemInfo.html', controller: 'ItemInfoCtrl' })
-  .when('/impressum', { templateUrl: 'general/page/impressum.html' })
-  .when('/legal', { templateUrl: 'general/page/legal.html' })
-  .when('/about', { templateUrl: 'general/page/about.html' })
-  .otherwise({ redirectTo: '/' })
-  ;
-});
+angular.module( "eve" )
+  .config( function( $routeProvider ) {
+    $routeProvider
+      .when( '/', {
+        templateUrl: 'general/page/main.html'
+      } )
+      .when( '/mining', {
+        redirectTo: '/mining/ore/sell'
+      } )
+      .when( '/mining/:kind/:pricetype', {
+        templateUrl: 'eve/page/mining.html',
+        controller: 'miningCtrl'
+      } )
+      .when( '/iteminfo', {
+        templateUrl: 'eve/page/itemInfo.html',
+        controller: 'ItemInfoCtrl'
+      } )
+      .when( '/impressum', {
+        templateUrl: 'general/page/impressum.html'
+      } )
+      .when( '/legal', {
+        templateUrl: 'general/page/legal.html'
+      } )
+      .when( '/about', {
+        templateUrl: 'general/page/about.html'
+      } )
+      .otherwise( {
+        redirectTo: '/'
+      } );
+  } );
