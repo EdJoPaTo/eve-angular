2c21dbd8955a50aafaa8b8735c5fa9749e589b09
Edgar To <edjopato@gmail.com>
2016-03-14 16:32:50 +0100

    buildCacheIndicator template in eigene html ausgelagert

--- modules/eve/directive/buildCacheIndicator.js
+++ modules/eve/directive/buildCacheIndicator.js
@@ -1,11 +1,7 @@
 angular.module( 'eve' )
   .directive( 'buildCacheIndicator', function() {
     return {
-      template: '<span ng-show="error">CREST: <span ng-show="error.statusText">{{ error.statusText }}</span><span ng-hide="error.statusText">connection failed</span></span>'
-      + '<span ng-hide="error">'
-      + '{{ itemcount }} Items loaded.'
-      + '<span ng-show="stillloading"> (still loading...)</span>'
-      + '</span>',
+      templateUrl: 'eve/directive/buildCacheIndicator.html',
       scope: {
         error: "=",
         itemcount: "=",
