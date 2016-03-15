8b1ae4df72db53c827728a3d83628a59fdce8f36
Edgar To <edjopato@gmail.com>
2016-03-15 14:06:04 +0100

    kann nun info über einen oder mehrere Crest Dienste zurück geben ($q.all)

--- modules/eve-crest/getCrestServiceInfo.js
+++ modules/eve-crest/getCrestServiceInfo.js
@@ -1,9 +1,19 @@
 angular.module( 'eve-crest' )
-  .factory( 'getCrestServiceInfo', function( httpCached ) {
-    return function( crestBaseUrl, serviceName ) {
+  .factory( 'getCrestServiceInfo', function( $q, httpCached ) {
+    function getSingleServiceInfo( crestBaseUrl, serviceName ) {
       return httpCached( crestBaseUrl )
         .then( function( request ) {
           return request.data[ serviceName ];
         } );
+    }
+
+    return function( crestBaseUrl, serviceName ) {
+      if ( typeof serviceName === 'string' ) {
+        return getSingleServiceInfo( crestBaseUrl, serviceName );
+      } else if ( Array.isArray( serviceName ) ) {
+        return $q.all( serviceName.map( name => getSingleServiceInfo( crestBaseUrl, name ) ) );
+      } else {
+        console.error( 'serviceName is of the wrong type' );
+      }
     };
   } );
