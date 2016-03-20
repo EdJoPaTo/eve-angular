ad0451e6597b9c98754ddbbe19f7324377e889c8
Edgar To <edjopato@gmail.com>
2016-03-20 18:51:27 +0100

    small layout changes

--- modules/z-killboard/directive/characterTableIskCell.js
+++ modules/z-killboard/directive/characterTableIskCell.js
@@ -1,12 +1,9 @@
 angular.module( 'z-killboard' )
   .directive( 'characterTableIskCell', function() {
     return {
-      template: '<span style="color: green;">{{ object.iskDestroyed | isk: true }}</span><br><span style="color:red;">{{ object.iskLost | isk: true }}</span>',
+      template: '<span class="successtext">{{ object.iskDestroyed | isk: true }}</span><br><br><span class="losstext">{{ object.iskLost | isk: true }}</span>',
       scope: {
         object: '='
-      },
-      link: function( scope, element, attr ) {
-        element.addClass( 'text-right' );
       }
     };
   } );
